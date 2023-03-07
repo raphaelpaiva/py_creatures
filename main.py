c93e0bc96788e824930b486420b9f7950f7f8048
raphaelpaiva <raphaeldpaiva@gmail.com>
2023-03-07 00:44:30 -0300

    Adding benchmark code

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ from creatures.render_system import RenderSystem
 
 DEFAULT_FILENAME = 'scenarios/somecreatures.yml'
 DEFAULT_FRAME_NUMBER = 'infinite'
+BENCHMARK_FRAME_NUMBER = 1000
 
 def main():
   filename = DEFAULT_FILENAME
   if len(sys.argv) > 1:
     filename = sys.argv[1]
+  
+  benchmark = len(sys.argv) > 2 and sys.argv[2] == '-b'
+
   
   try:
     frame = Loader(filename).load()
@@ -21,9 +25,10 @@ def main():
     render_system = RenderSystem(world)
     world.add_system(render_system)
     
-    while True:
-      handle_events()
-      world.update()
+    if benchmark:
+      benchmark_loop(world)
+    else:
+      infinite_loop(world)
 
   except ParseException as e:
     print(e)
@@ -31,6 +36,21 @@ def main():
   except Exception as e:
     traceback.print_exc() #sys.exc_info()[2]
     exit(255)
+
+def infinite_loop(world):
+  while True:
+    handle_events()
+    world.update()
+
+def benchmark_loop(world):
+  print('Benchmarking...')
+  progress = 0
+  for frame in range(BENCHMARK_FRAME_NUMBER):
+    progress = 100 * frame / BENCHMARK_FRAME_NUMBER
+    print(progress)
+    
+    handle_events()
+    world.update()
 
 def time_ms():
     return time() / 1000 
